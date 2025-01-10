22716e6234a6f89278a680705fd5702cbd8e9c93
vinit0019 <marthakvinit5@gmail.com>
2025-01-10 16:52:31 +0530

    comit

---
+++ one/day1-1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    cout << "enter a value" << endl;
+    cin >> a;
+
+    cout << "enter b value" << endl;
+    cin >> b;
+
+    cout << "entered value of a " << a << endl;
+
+    c=a+b;
+    cout << c << endl;
+    
+    return 0;
+}
---
+++ one/day1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int b=20;
+    int c=b-a;
+
+    cout << c << endl;
+
+    char e[6]="vinit";
+    char f='v';
+
+    cout << e << endl;
+
+    cout << e[0] << endl;
+    cout << e[1] << endl;
+    cout << e[2] << endl;
+    cout << e[3] << endl;
+    cout << e[4] << endl;
+
+    return 0;
+}
