9ccc60ebf888e9bfd552f6a4d63460dcd1184c23
vinit0019 <marthakvinit5@gmail.com>
2025-01-24 16:15:01 +0530

    Some CHange

---
+++ Day-4/day4.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Arithmetic Operators
+
+    // int a = 20;
+    // int b = 10;
+
+    // cout << a+b << endl;
+
+    // cout << a-b << endl;
+
+    // cout << a*b << endl;
+
+    // cout << a/b << endl;
+
+    // cout << a%b << endl;
+
+
+ 
+    // sum  C=A+B
+    // int a ;
+    // int b ;
+    // int c = a+b;
+
+    // cout << "enter a value" << endl;
+    // cin >> a;
+
+    // cout << "enter b value" << endl;
+    // cin >> b;
+
+    // cout << a+b << endl;
+
+
+
+    // Relational Oprators
+
+    // int a = 50;
+    // int b = 60;
+
+    // if (a > b){
+    // cout << "a is grater than" << endl;
+    // }
+    // else{
+    // cout << "b is grater than" << endl;
+    // }
+    
+    // int a = 90;
+    // int b = 90;
+
+    // if (a >= b){
+    // cout << "a is grater than" << endl;
+    // }
+    // else{
+    // cout << "b is grater than" << endl;
+    // }
+
+
+
+    // Logical operator 
+
+    // int a = 5;
+    // int b = 8;
+
+    // cout << ((b>a)&&(b>a)) << endl; 
+    // cout << ((b>a)||(a>b)) << endl; 
+    // cout << !((a>b)&&(a>b)) << endl; 
+
+
+
+    // Assignment Oprators
+
+    // int a = 100;
+    // int b = 20;
+    //  a += b;
+    // cout << a << endl;
+
+    //  int a = 100;
+    // int b = 20;
+    //  a -= b;
+    // cout << a << endl;
+
+    // int a = 100;
+    // int b = 20;
+    //  a *= b;
+    // cout << a << endl;
+
+    // int a = 100;
+    // int b = 20;
+    //  a /= b;
+    // cout << a << endl;
+
+    // int a = 100;
+    // int b = 20;
+    //  a %= b;
+    // cout << a << endl;
+
+
+
+    // Unary operator
+
+    
+return 0 ;
+}
